a9041b2671779fd4afee91db7f163b73e0e36746
ikedam <devld@ikedam.jp>
2021-05-16 17:08:47 +0900

    Implementation for list partitions

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,35 @@
 #include <Arduino.h>
+#include <M5EPD.h>
+
+namespace {
+  void dump_partition(esp_partition_type_t type) {
+  Serial.printf("label type subtype encrypted address-start address-end size:\n");
+    esp_partition_iterator_t iter = esp_partition_find(type, ESP_PARTITION_SUBTYPE_ANY, NULL);
+    while(iter != NULL) {
+      const esp_partition_t* part = esp_partition_get(iter);
+      Serial.printf(
+        "%s %02x %02x %d 0x%06x 0x%06x %d\n",
+        part->label,
+        part->type,
+        part->subtype,
+        part->encrypted,
+        part->address,
+        part->address + part->size,
+        part->size
+      );
+      iter = esp_partition_next(iter);
+    }
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  M5.begin(false, false, true, false, true);
+
+  Serial.printf("APP partitions:\n");
+  dump_partition(ESP_PARTITION_TYPE_APP);
+
+  Serial.printf("DATA partitions:\n");
+  dump_partition(ESP_PARTITION_TYPE_DATA);
 }
 
 void loop() {
